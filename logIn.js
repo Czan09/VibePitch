8e147a723d4685e9057ccda72164ccaf744d1d92
Czan <mansinghshrijan@gmail.com>
2025-09-02 23:01:27 +0545

    added (homePage, logIn.js)

---
+++ logIn.js
@@ -0,0 +1,27 @@
+const form = document.getElementById("join-form");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const confirmPassword=document.getElementById("confirm-password").value;
+  if(confirmPassword !== password){
+    
+  }
+  try {
+    const response = await fetch("http://localhost:3000/add-user", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, email, password })
+    });
+
+    const data = await response.json();
+    console.log(data);
+    alert(data.success ? "User created! ID: " + data.userId : data.message);
+  } catch (err) {
+    console.error(err);
+    alert("Error sending request");
+  }
+});
